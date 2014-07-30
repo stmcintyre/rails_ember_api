a6cad0eec8511a7f10bf65c8dc28794f1375e80f
Steven McIntyre <steven.mcintyre@injixo.com>
2014-07-30 14:13:53 +0100

    Permit contact params

--- app/admin/contact.rb
+++ app/admin/contact.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register Contact do
 
+  permit_params :first_name, :last_name, :email, :title
 
+  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
